f808a25a4daa697ffcc98d6e813606fd7a91fd9c
Llane00 <bluetinestyle@126.com>
2019-10-22 11:32:26 +0800

    add: 完善printNarcissisticNumber函数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,7 +14,8 @@ public class Main {
      */
     public static void printNarcissisticNumber() {
         for (int i = 100; i <= 999; i++) {
-            if (i == Math.pow(i / 100, 3) + Math.pow(i % 100 / 10, 3) + Math.pow(i % 100 % 10, 3)) {
+            int sum = (int)(Math.pow(i / 100, 3) + Math.pow(i % 100 / 10, 3) + Math.pow(i % 10, 3));
+            if (i == sum) {
                 System.out.println(i);
             }
         }
