dad64cfe2474e59e9e7e65807bed7ff7ca09014d
linweikun95 <63492316+linweikun95@users.noreply.github.com>
2020-06-20 15:38:09 +0800

    feat: get narcissistic (#190)
    feat: get narcissistic (#190)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,19 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        // 第一位
+        for (int i = 1; i < 10; i++) {
+            // 第二位
+            for (int j = 0; j < 10; j++) {
+                // 第三位
+                for (int k = 0; k < 10; k++) {
+                    int num = i * 100 + j * 10 + k;
+                    if (k * k * k + j * j * j + i * i * i == num) {
+                        System.out.println(num);
+                    }
+                }
+            }
+        }
+    }
 }
