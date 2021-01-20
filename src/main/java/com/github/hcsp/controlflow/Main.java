0ad88685fe0b42540cfafedeb95fb624b1085946
zeq96 <zeq_96@163.com>
2021-01-21 04:44:35 +0800

    实现输出水仙花数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,14 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            int g = i % 10;
+            int s = (i % 100) / 10;
+            int b = (i % 1000) / 100;
+            if (Math.pow(g, 3) + Math.pow(s, 3) +Math.pow(b, 3) == i) {
+                System.out.println(i);
+            }
+        }
+    }
 }
