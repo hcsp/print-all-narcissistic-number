73d2f7bac49ac48488566b28016035474ce667be
soohea <77014206+soohea@users.noreply.github.com>
2021-09-08 12:18:19 +0800

    print-all-narcissistic-number (#233)
     print-all-narcissistic-number (#233)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,17 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            int digit = i % 10;
+            int ten = (i / 10) % 10;
+            int hundred = i / 100;
+            if (i == digit * digit * digit + ten * ten * ten + hundred * hundred * hundred) {
+                System.out.println(i);
+            }
+
+
+        }
+
+    }
 }
