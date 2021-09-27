8a381f054a6a42abe1a75314af38d0956d17b139
DEMAO <987854018@qq.com>
2021-09-27 17:59:50 +0800

    打印所有的水仙花数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -6,11 +6,22 @@ public class Main {
     }
 
     /**
-     * 打印所有的水仙花数。水仙花数是指一个三位数，其各位数字立方和等于该数本身。
+     * 打印所有的水仙花数。水仙花数是指一个三位数，其各位数字立方和等于该数本身。  100-999
      *
-     * <p>例如，153是一个水仙花数，因为1的立方+5的立方+3的立方 = 153
+     * <p>例如，153是一个水仙花数，因为1的立方+5的立方+3的立方 = 153   153/100
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        int i = 100;
+        while (i < 1000) {
+            int a = i / 100;
+            int b = i % 100 / 10;
+            int c = i % 10;
+            if (Math.pow(a,3) + Math.pow(b,3) + Math.pow(c,3) == i) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
 }
