b474b50067f1da805171eba7dd954cfda1a5d3e2
XIANG CHEN <58834456+futrue1024@users.noreply.github.com>
2022-07-02 16:00:50 +0800

    print-all-narcissistic-number (#266)
    print-all-narcissistic-number (#266)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,14 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            int a = i % 10;
+            int b = ((i - a) / 10) % 10;
+            int c = (i - b * 10 - a) / 100;
+            if (a * a * a + b * b * b + c * c * c == i) {
+                System.out.println(i);
+            }
+        }
+    }
 }
