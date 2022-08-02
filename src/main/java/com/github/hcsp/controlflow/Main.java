ff7f629c9429c98d831687daa3f5db2af9f55d45
yangbisheng <2606424906@qq.com>
2022-08-03 03:56:04 +0800

    print all narcissistic number (#267)
    print all narcissistic number (#267)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,15 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            double a = i / 100;
+            double b = (i % 100) / 10;
+            double c = i % 10;
+            if ((Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3)) == i) {
+                System.out.println(i);
+            }
+
+        }
+    }
 }
